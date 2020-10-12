a007ca8b0594bc4b677ff77c050640c12b368715
Stuart Forrest <stuart.forrest@infinityworks.com>
2020-10-12 19:22:57 +0100

    use valid json for the return message

--- apitrigger/main.go
+++ apitrigger/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -11,27 +12,49 @@ import (
 	"github.com/aws/aws-sdk-go/service/apigatewaymanagementapi"
 )
 
+type body struct {
+	Message string `json:"message"`
+}
+
+type message struct {
+	Message string `json:"message"`
+}
+
 // Handle is
 func Handle(_ context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	fmt.Println("api-trigger handler ran")
-	fmt.Println(fmt.Sprintf("%+v", request))
+
 	sess, _ := session.NewSession(&aws.Config{
 		Region: aws.String("eu-west-2"),
 	})
 	connectionID, ok := request.PathParameters["connectionID"]
 	if !ok {
 		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body: "connectionID must be provided",
+			StatusCode: 400,
+			Body:       "connectionID must be provided",
 		}, nil
 	}
+	var b body
+	err := json.Unmarshal([]byte(request.Body), &b)
+	if err != nil {
+		fmt.Println("body unmarshal error")
+		fmt.Println(err)
+	}
+	m, err := json.Marshal(message{
+		Message: b.Message,
+	})
+	if err != nil {
+		fmt.Println("marshalling error")
+		fmt.Println(err)
+	}
+
 	url := fmt.Sprintf("https://mudkgw5pwb.execute-api.eu-west-2.amazonaws.com/%s", request.RequestContext.Stage)
 	apigw := apigatewaymanagementapi.New(sess, &aws.Config{
 		Endpoint: aws.String(url),
 	})
-	_, err := apigw.PostToConnection(&apigatewaymanagementapi.PostToConnectionInput{
+	_, err = apigw.PostToConnection(&apigatewaymanagementapi.PostToConnectionInput{
 		ConnectionId: aws.String(connectionID),
-		Data:         []byte(fmt.Sprintf(`{"from the API": true}`)),
+		Data:         []byte(m),
 	})
 	if err != nil {
 		fmt.Println("error")
