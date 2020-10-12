b7b205f6d694001320c56fb4741767d7dac173c2
Stuart Forrest <stuart.forrest@infinityworks.com>
2020-10-12 19:55:44 +0100

    make api package

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-env GOOS=linux go build -o bin/loopback ./loopback
-env GOOS=linux go build -o bin/connectionmanager ./connectionmanager
-env GOOS=linux go build -o bin/apitrigger ./apitrigger
+env GOOS=linux go build -o bin/loopback ./api/loopback
+env GOOS=linux go build -o bin/connectionmanager ./api/connectionmanager
+env GOOS=linux go build -o bin/apitrigger ./api/apitrigger
